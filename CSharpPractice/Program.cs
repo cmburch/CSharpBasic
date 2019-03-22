a7fbbb1b2873d2508c76e367999557d7a1880296
Charles Burch <georgia_beast89@yahoo.com>
2019-03-22 11:47:49 -0500

    testing type is C#

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -7,6 +7,14 @@ namespace CSharpPractice
         public static void Main(string[] args)
         {
            Console.WriteLine("Hello World!");
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.15f;
+            char letter = 'H';
+            string firstName = "Name";
+            bool isWorking = true;
+
+            Console.Write(number);
         }
     }
 }
