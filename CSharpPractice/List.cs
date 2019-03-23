4acfb8a2802eb7106e54759388748ac9ee0f569a
Charles Burch <georgia_beast89@yahoo.com>
2019-03-23 15:40:32 -0500

    created a method to reverse a name

---
+++ CSharpPractice/List.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collection.Generic;
+
+namespace CSharpPractice
+{
+    public class List
+    {
+        var number = new List<int> {1,2,3,4};
+        number.Add
+    }
+}
